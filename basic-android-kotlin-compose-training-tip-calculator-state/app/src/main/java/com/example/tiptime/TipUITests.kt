eb25418152ebf66b541fc13f5c8ac94e87711c8b
YuichiYH <64170401+YuichiYH@users.noreply.github.com>
2023-02-01 16:57:21 -0300

    Test Answers

---
+++ basic-android-kotlin-compose-training-tip-calculator-state/app/src/main/java/com/example/tiptime/TipUITests.kt
@@ -0,0 +1,26 @@
+package com.example.tiptime
+import com.example.tiptime.ui.theme.TipTimeTheme
+import org.junit.Rule
+import org.junit.Test
+
+class TipUITests {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
+    @Test
+    fun calculate_20_percent_tip() {
+        composeTestRule.setContent {
+            TipTimeTheme {
+                TipTimeScreen()
+            }
+        }
+        composeTestRule.onNodeWithText("Bill Amount")
+            .performTextInput("10")
+        composeTestRule.onNodeWithText("Tip (%)").performTextInput("20")
+        val expectedTip = java.text.NumberFormat.getCurrencyInstance().format(2)
+        composeTestRule.onNodeWithText("Tip Amount: $expectedTip").assertExists(
+            "No node with this text was found."
+        )
+    }
+}
