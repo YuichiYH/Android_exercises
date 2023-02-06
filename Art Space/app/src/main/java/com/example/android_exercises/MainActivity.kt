94c01cc2f20232b3115bc9a048e5c1b42c0547df
YuichiYH <64170401+YuichiYH@users.noreply.github.com>
2023-02-06 16:52:26 -0300

    created function ShowArt

--- Art Space/app/src/main/java/com/example/android_exercises/MainActivity.kt
+++ Art Space/app/src/main/java/com/example/android_exercises/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.android_exercises
 
+import android.graphics.Paint
 import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.android_exercises.ui.theme.Android_exercisesTheme
 
@@ -24,9 +30,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    ArtWork_App()
                 }
             }
         }
+    }
+}
+
+@Preview
+@Composable
+fun app_preview(){
+    ArtWork_App()
+}
+
+@Composable
+fun ArtWork_App(){
+
+    var state by remember {
+        mutableStateOf(0)
+    }
+
+    val paintings = when(state%3) {
+        0 -> R.drawable.vangogh
+        1 -> R.drawable.michelangelo
+        else -> R.drawable.leonardo_da_vinci
+    }
+
+    val description = when(state%3) {
+        0 -> R.string.vangogh
+        1 -> R.string.michelangelo
+        else -> R.string.leonardo
+    }
+
+    ShowArt(image = paintings,
+        description = description
+    )
+}
+
+@Composable
+fun ShowArt(image: Int, description: Int){
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = image), contentDescription = stringResource(id = description))
     }
 }
